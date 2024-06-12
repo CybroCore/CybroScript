5906dc6367b6466ba4f5886cc2b9423bfe4b6aa2
Simon Ferns <122154257+AdonisCodes@users.noreply.github.com>
2024-06-12 13:02:28 +0200

    Update cybro.swift

--- cybro.swift
+++ cybro.swift
@@ -1,5 +1,7 @@
 import Foundation
 
+// https://craftinginterpreters.com/scanning.html#reserved-words-and-identifiers
+
 enum TokenType {
   case LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE
   case COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR
@@ -24,6 +26,141 @@ class Token {
 
   func toString() -> String {
     return "Token(type: \(type), lexeme: \(lexeme), literal: \(literal ?? "nil"))"
+  }
+}
+
+class Scanner {
+  final let source: String
+  var tokens: [Token]
+  private var start: Int = 0
+  private var current: Int = 0
+  private var line: Int = 1
+
+  init(source: String) {
+    self.source = source
+  }
+
+  func scanTokens() -> [Token] {
+    while !self.isAtEnd() {
+      start = current
+      self.scanToken()
+    }
+
+    tokens.append(Token(type: .EOF, lexeme: "", literal: nil, line: line))
+    return tokens
+  }
+
+  func isAtEnd() -> Bool {
+    current >= source.length();
+  }
+
+  func scanToken() {
+    let c: String = advance();
+    switch (c) {
+      case "(": addToken(.LEFT_PAREN); break;
+      case ")": addToken(.RIGHT_PAREN); break;
+      case "{": addToken(.LEFT_BRACE); break;
+      case "{": addToken(.RIGHT_BRACE); break;
+      case ",": addToken(.COMMA); break;
+      case ".": addToken(.DOT); break;
+      case "-": addToken(.MINUS); break;
+      case "+": addToken(.PLUS); break;
+      case ";": addToken(.SEMICOLON); break;
+      case "*": addToken(.STAR); break; 
+      case "!":
+        addToken(match("=") ? .BANG_EQUAL : .BANG);
+        break;
+      case "=":
+        addToken(match("=") ? .EQUAL_EQUAL : .EQUAL);
+        break;
+      case "<":
+        addToken(match("=") ? .LESS_EQUAL : .LESS);
+        break;
+      case ">":
+        addToken(match("=") ? .GREATER_EQUAL : .GREATER);
+        break;
+      case "/":
+        if (match("/")) {
+          while (peek() != "\n" && !isAtEnd()) { advance() }
+        } else {
+          addToken(.SLASH);
+        }
+        break;
+      case "\"": string(); break;
+      default: 
+        if (isDigit(c)) { 
+          number()
+          break
+        }
+        Cybro().error(line: self.line, message: "Unexpected character \(c)")
+        break;
+    }
+  }
+
+  func isDigit(c: String) -> Bool {
+    return c >= "0" && c <= "9"
+  }
+
+  func number() {
+    while (isDigit(peek())) { advance() };
+
+    if (peek() == "." && isDigit(peekNext())) {
+      advance();
+
+      while (isDigit(peek())) { advance() };
+    }
+
+    addToken(.NUMBER, Double(lexeme)!)
+  }
+
+  func peekNext() -> String {
+    if (isAtEnd()) { return "\0" }
+    return source.substring(with: Index(encodedOffset: current + 1))
+  }
+
+  func string() {
+    while peek != "\"" && !isAtEnd() {
+      if peek == "\n" { line += 1 }
+      advance()
+    }
+
+    if (isAtEnd()) {
+      Cybro().error(line: self.line, message: "Unterminated string.");
+      return
+    }
+
+    advance()
+
+    let value = source.substring(with: start + 1, to: current - 1)
+    addToken(.STRING, value)
+  }
+
+  func match(expected: String) -> Bool {
+    if self.isAtEnd() { return false }
+    if source.substring(with: Range(self.current..<self.current + expected.length())) != expected { return false }
+    self.current += 1
+    return true
+  }
+
+  func peek() -> String {
+    if self.isAtEnd() { return "\0" }
+    return source.substring(with: Range(self.current..<self.current + 1))
+    
+  }
+
+  func advance() -> String {
+    let value = self.source[self.current];
+    self.current += 1
+    return value
+  }
+
+  func addToken(_ type: TokenType) {
+    addToken(type, nil)
+  }
+
+  func addToken(_ type: TokenType, literal: Any?) {
+    let text: String = self.source.substring(with: self.start..<self.current)
+    tokens.append(Token(type: type, lexeme: text, literal: literal, line: line))
   }
 }
 
